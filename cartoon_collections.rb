54c5ca616358c4bb9839463630ad2c6e2d244203
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 12:43:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,7 @@
 def roll_call_dwarves(dwarves)
-  i = 0
-  collection = []
-  while i < dwarves.length
-    puts "1. #{dwarves[i]}"
-    i += 1
-  end
+  dwarves.each_with_index { |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  }
 end
 
 
